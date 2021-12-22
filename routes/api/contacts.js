249141f53bf4774e8533a47b2f08bd823bbdc9d8
Sergii Budak <86181734+corund1976@users.noreply.github.com>
2021-12-22 14:35:48 +0200

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,74 +1,22 @@
 const express = require('express')
 const router = express.Router()
 
-const { validateAddContact, validateUpdateContact } = require('../../middlewares/validation')
+const contactsControllers = require('../../controllers/controllers')
+const contactsValidation = require('../../middlewares/validation')
 
-const {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-} = require('../../model/index')
+router.get('/', async (req, res, next) =>
+  await contactsControllers.listContacts(req, res, next))
 
-router.get('/', async (req, res, next) => {
-  try {
-    const result = await listContacts()
+router.get('/:contactId', async (req, res, next) =>
+  await contactsControllers.getContactById(req, res, next))
 
-    if (typeof result === 'string') return res.status(500).json(result)
+router.post('/', contactsValidation.validateAddContact, async (req, res, next) =>
+  await contactsControllers.addContact(req, res, next))
 
-    return res.json(result)
-  } catch (error) {
-    next(error)
-  }
-})
+router.delete('/:contactId', async (req, res, next) =>
+  await contactsControllers.removeContact(req, res, next))
 
-router.get('/:contactId', async (req, res, next) => {
-  try {
-    const result = await getContactById(req.params.contactId)
-
-    if (typeof result === 'string') return res.status(500).json(result)
-
-    return res.json(result)
-  } catch (error) {
-    next(error)
-  }
-})
-
-router.post('/', validateAddContact, async (req, res, next) => {
-  try {
-    const result = await addContact(req.body)
-
-    if (typeof result === 'string') return res.status(500).json(result)
-
-    return res.status(201).json(result)
-  } catch (error) {
-    next(error)
-  }
-})
-
-router.delete('/:contactId', async (req, res, next) => {
-  try {
-    const result = await removeContact(req.params.contactId)
-
-    if (typeof result === 'string') return res.status(500).json(result)
-
-    return res.json({ message: `✅ Contact id: ${req.params.contactId} deleted` })
-  } catch (error) {
-    next(error)
-  }
-})
-
-router.patch('/:contactId', validateUpdateContact, async (req, res, next) => {
-  try {
-    const result = await updateContact(req.params.contactId, req.body)
-
-    if (typeof result === 'string') return res.status(500).json(result)
-
-    return res.json(result)
-  } catch (error) {
-    next(error)
-  }
-})
+router.patch('/:contactId', contactsValidation.validateUpdateContact, async (req, res, next) =>
+  await contactsControllers.updateContact(req, res, next))
 
 module.exports = router
